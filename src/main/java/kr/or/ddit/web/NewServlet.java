036a31c46c47b33330fa4a9a58d6a1c161f4863a
ymj <dbalswl4090@gmail.com>
2018-10-10 18:29:04 +0900

    newBranch 새로운 기능 개발중

---
+++ src/main/java/kr/or/ddit/web/NewServlet.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+// 새로운 기능 요청에 의한 새로운 서블릿 생성 
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/NewServlet")
+public class NewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
